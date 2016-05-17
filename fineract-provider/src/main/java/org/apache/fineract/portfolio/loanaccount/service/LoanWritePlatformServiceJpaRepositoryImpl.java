0cacebcbac9c760a6783f6d6a455966990b9ddf0
sachinkulkarni12 <sachin.kulkarni@confluxtechnologies.com>
2016-05-17 17:16:58 +0530

    posting GL entry for new transaction while apply meeting changes

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanWritePlatformServiceJpaRepositoryImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanWritePlatformServiceJpaRepositoryImpl.java
@@ -2056,6 +2056,7 @@ public class LoanWritePlatformServiceJpaRepositoryImpl implements LoanWritePlatf
                 }
 
                 saveLoanWithDataIntegrityViolationChecks(loan);
+                postJournalEntries(loan, existingTransactionIds, existingReversedTransactionIds);
             }
         }
     }
