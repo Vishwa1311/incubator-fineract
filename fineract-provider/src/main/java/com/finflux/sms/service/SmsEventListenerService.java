275eedd7cad9d19a408c3b0793d3b8046c016b6b
satish-conflux <satish.sajjan@confluxtechnologies.com>
2016-10-07 16:58:25 +0530

    RM 1962 : SMS integration for loan disbursal and repayment transactions

--- fineract-provider/src/main/java/com/finflux/sms/service/SmsEventListenerService.java
+++ fineract-provider/src/main/java/com/finflux/sms/service/SmsEventListenerService.java
@@ -1,11 +1,8 @@
 package com.finflux.sms.service;
 
-import org.apache.fineract.portfolio.loanaccount.domain.Loan;
 import org.apache.fineract.portfolio.loanaccount.domain.LoanTransaction;
 
 public interface SmsEventListenerService {
 
-    public void sendMessgeProcessForLoanDisbursal(final Loan loan);
-
-    public void sendMessgeProcessForRepayment(final LoanTransaction loanTransaction);
+    public void sendMessgeProcessForLoanTransaction(final LoanTransaction loanTransaction, String messageTemplate);
 }
--- fineract-provider/src/main/java/com/finflux/sms/service/SmsEventListenerServiceImpl.java
+++ fineract-provider/src/main/java/com/finflux/sms/service/SmsEventListenerServiceImpl.java
@@ -25,11 +25,12 @@ import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import com.finflux.sms.domain.ProductType;
-import com.finflux.sms.domain.SmsTransactionType;
+import com.finflux.sms.domain.SMSSupportedProductType;
+import com.finflux.sms.domain.SMSSupportedTransactionType;
 
 @Service
 public class SmsEventListenerServiceImpl implements SmsEventListenerService {
@@ -58,86 +59,42 @@ public class SmsEventListenerServiceImpl implements SmsEventListenerService {
                 new SmsSchedulaEventListnerForRepayment());
     }
 
-    @Override
-    public void sendMessgeProcessForLoanDisbursal(final Loan loan) {
-        if (loan.client().getMobileNo() != null) {
-            final Integer transactionType = SmsTransactionType.DISBURSEMENT.getValue();
-            final Integer productType = ProductType.LOANPRODUCT.getValue();
-            final Long productId = loan.productId();
-            String messageTemplate = getSmsMessageTemplate(transactionType, productType, productId);
-            if (messageTemplate != null && !messageTemplate.equalsIgnoreCase("null") && messageTemplate.length() > 0) {
-                // Dear {{clientName}}, Your loan amount {{transactionAmount}}
-                // was disbursed successfully on {{transactionDate}}, Please
-                // note your loan account number {{accountNumber}}
-                final String clientName = loan.client().getDisplayName();
-                final Money transactionAmount = loan.getPrincpal();
-                final LocalDate transactionDate = loan.getDisbursementDate();
-                final String accountNumber = loan.getAccountNumber();
-                messageTemplate = messageTemplate.replace("{{clientName}}", clientName);
-                messageTemplate = messageTemplate.replace("{{transactionAmount}}", transactionAmount.toString());
-                messageTemplate = messageTemplate.replace("{{transactionDate}}", transactionDate.toString());
-                messageTemplate = messageTemplate.replace("{{accountNumber}}", accountNumber.toString());
-                sendMessge(messageTemplate, loan);
-            }
-        }
-    }
-
-    @SuppressWarnings("unused")
-    private void sendMessge(final String message, final Loan loan) {
-        final SmsMessage smsMessage = SmsMessage.pendingSms(null, null, null, loan.group(), loan.client(), loan.loanOfficer(), message,
-                null, loan.client().getMobileNo(), null);
-        this.smsMessageRepository.save(smsMessage);
-        Long groupId = null;
-        if (loan.group() != null) {
-            groupId = loan.group().getId();
-        }
-        Long clientId = null;
-        if (loan.client() != null) {
-            clientId = loan.client().getId();
-        }
-        Long staffId = null;
-        if (loan.loanOfficer() != null) {
-            staffId = loan.loanOfficer().getId();
-        }
-        final EnumOptionData status = SmsMessageEnumerations.status(smsMessage.getStatusType());
-        final SmsData smsData = SmsData.instance(smsMessage.getId(), smsMessage.getExternalId(), groupId, clientId, staffId, status,
-                smsMessage.getSourceAddress(), smsMessage.getMobileNo(), message, smsMessage.getCampaignName(),
-                new LocalDate(smsMessage.getSubmittedOnDate()));
-        final Collection<SmsData> pendingMessages = new ArrayList<SmsData>();
-        pendingMessages.add(smsData);
-        TenantSmsConfiguration tenantSmsConfiguration = null;
-        this.smsMessageScheduledJobService.sendMessagesProcess(tenantSmsConfiguration, pendingMessages);
-    }
-
-    private String getSmsMessageTemplate(final Integer transactionType, final Integer productType, final Long productId) {
-        final StringBuilder sqlBuilder = new StringBuilder(10);
-        sqlBuilder.append("SELECT pstyc.message_template AS messageTemplate ");
-        sqlBuilder.append("FROM f_product_sms_configuration psmc ");
-        sqlBuilder.append("JOIN f_product_sms_transaction_type_config pstyc ON pstyc.type = ? ");
-        sqlBuilder.append("AND pstyc.product_sms_config_id = psmc.id AND pstyc.is_active = 1 ");
-        sqlBuilder.append("WHERE psmc.is_active = 1 AND psmc.is_enabled = 1 AND psmc.product_type = ? AND psmc.product_id = ? ");
-        return this.jdbcTemplate.queryForObject(sqlBuilder.toString(), new Object[] { transactionType, productType, productId },
-                String.class);
-    }
-
     private class SmsSchedulaEventListnerForLoanDisbursal implements BusinessEventListner {
 
         @SuppressWarnings("unused")
         @Override
         public void businessEventToBeExecuted(Map<BUSINESS_ENTITY, Object> businessEventEntity) {
             // TODO Auto-generated method stub
-
         }
 
         @Override
         public void businessEventWasExecuted(Map<BUSINESS_ENTITY, Object> businessEventEntity) {
-            Object loanEntity = businessEventEntity.get(BUSINESS_ENTITY.LOAN);
-            if (loanEntity != null) {
-                Loan loan = (Loan) loanEntity;
-                sendMessgeProcessForLoanDisbursal(loan);
+            Object loanTransactionEntity = businessEventEntity.get(BUSINESS_ENTITY.LOAN_TRANSACTION);
+            if (loanTransactionEntity != null) {
+                final LoanTransaction loanTransaction = (LoanTransaction) loanTransactionEntity;
+                final Integer transactionType = SMSSupportedTransactionType.DISBURSEMENT.getValue();
+                final Integer productType = SMSSupportedProductType.LOANPRODUCT.getValue();
+                final Long productId = loanTransaction.getLoan().productId();
+                String messageTemplate = getSmsMessageTemplate(transactionType, productType, productId);
+                sendMessgeProcessForLoanTransaction(loanTransaction, messageTemplate);
             }
         }
+    }
 
+    private String getSmsMessageTemplate(final Integer transactionType, final Integer productType, final Long productId) {
+        try {
+            final StringBuilder sqlBuilder = new StringBuilder(10);
+            sqlBuilder.append("SELECT pstyc.message_template AS messageTemplate ");
+            sqlBuilder.append("FROM f_product_sms_configuration psmc ");
+            sqlBuilder.append("JOIN f_product_sms_transaction_type_config pstyc ON pstyc.type = ? ");
+            sqlBuilder.append("AND pstyc.product_sms_config_id = psmc.id AND pstyc.is_active = 1 ");
+            sqlBuilder.append("WHERE psmc.is_active = 1 AND psmc.is_enabled = 1 AND psmc.product_type = ? AND psmc.product_id = ? ");
+            return this.jdbcTemplate.queryForObject(sqlBuilder.toString(), new Object[] { transactionType, productType, productId },
+                    String.class);
+        } catch (EmptyResultDataAccessException e) {
+
+        }
+        return null;
     }
 
     private class SmsSchedulaEventListnerForRepayment implements BusinessEventListner {
@@ -146,7 +103,6 @@ public class SmsEventListenerServiceImpl implements SmsEventListenerService {
         @Override
         public void businessEventToBeExecuted(Map<BUSINESS_ENTITY, Object> businessEventEntity) {
             // TODO Auto-generated method stub
-
         }
 
         @Override
@@ -154,20 +110,20 @@ public class SmsEventListenerServiceImpl implements SmsEventListenerService {
             Object loanTransactionEntity = businessEventEntity.get(BUSINESS_ENTITY.LOAN_TRANSACTION);
             if (loanTransactionEntity != null) {
                 final LoanTransaction loanTransaction = (LoanTransaction) loanTransactionEntity;
-                sendMessgeProcessForRepayment(loanTransaction);
+                final Integer transactionType = SMSSupportedTransactionType.REPAYMENT.getValue();
+                final Integer productType = SMSSupportedProductType.LOANPRODUCT.getValue();
+                final Long productId = loanTransaction.getLoan().productId();
+                String messageTemplate = getSmsMessageTemplate(transactionType, productType, productId);
+                sendMessgeProcessForLoanTransaction(loanTransaction, messageTemplate);
             }
         }
 
     }
 
     @Override
-    public void sendMessgeProcessForRepayment(final LoanTransaction loanTransaction) {
+    public void sendMessgeProcessForLoanTransaction(final LoanTransaction loanTransaction, String messageTemplate) {
         final Loan loan = loanTransaction.getLoan();
         if (loan.client().getMobileNo() != null) {
-            final Integer transactionType = SmsTransactionType.REPAYMENT.getValue();
-            final Integer productType = ProductType.LOANPRODUCT.getValue();
-            final Long productId = loan.productId();
-            String messageTemplate = getSmsMessageTemplate(transactionType, productType, productId);
             if (messageTemplate != null && !messageTemplate.equalsIgnoreCase("null") && messageTemplate.length() > 0) {
                 /*
                  * Dear {{clientName}}, Your EMI amount {{transactionAmount}}
@@ -186,6 +142,37 @@ public class SmsEventListenerServiceImpl implements SmsEventListenerService {
                 messageTemplate = messageTemplate.replace("{{accountNumber}}", accountNumber.toString());
                 sendMessge(messageTemplate, loan);
             }
+        }
+    }
+
+    @SuppressWarnings("unused")
+    private void sendMessge(final String message, final Loan loan) {
+        try {
+            final SmsMessage smsMessage = SmsMessage.pendingSms(null, null, null, loan.group(), loan.client(), loan.loanOfficer(), message,
+                    null, loan.client().getMobileNo(), null);
+            this.smsMessageRepository.save(smsMessage);
+            Long groupId = null;
+            if (loan.group() != null) {
+                groupId = loan.group().getId();
+            }
+            Long clientId = null;
+            if (loan.client() != null) {
+                clientId = loan.client().getId();
+            }
+            Long staffId = null;
+            if (loan.loanOfficer() != null) {
+                staffId = loan.loanOfficer().getId();
+            }
+            final EnumOptionData status = SmsMessageEnumerations.status(smsMessage.getStatusType());
+            final SmsData smsData = SmsData.instance(smsMessage.getId(), smsMessage.getExternalId(), groupId, clientId, staffId, status,
+                    smsMessage.getSourceAddress(), smsMessage.getMobileNo(), message, smsMessage.getCampaignName(), new LocalDate(
+                            smsMessage.getSubmittedOnDate()));
+            final Collection<SmsData> pendingMessages = new ArrayList<SmsData>();
+            pendingMessages.add(smsData);
+            TenantSmsConfiguration tenantSmsConfiguration = null;
+            this.smsMessageScheduledJobService.sendMessagesProcess(tenantSmsConfiguration, pendingMessages);
+        } catch (Exception e) {
+
         }
     }
 }
